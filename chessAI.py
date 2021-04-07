b295758051937b52352c06e0bccdb1c9fecf8375
BTran3706 <75403122+BTran3706@users.noreply.github.com>
2021-04-07 14:29:41 -0700

    Update chessAI.py

--- chessAI.py
+++ chessAI.py
@@ -4,12 +4,12 @@ from chessboard import display
 def evaluateScore():
     
     if board.is_checkmate():
-        if board.turn: #If White turn, return -9999 meaning Black won. Else return 9999 meaning White won
+        if board.turn: #If White turn return -9999 meaning Black won. Else return 9999 meaning White won
             return -9999
         else:
             return 9999
 
-    #Score pieces based on their position. Piece square tables can be found online
+    #Score pieces based on position. Piece square tables found online
     pawnTable = [
         0, 0, 0, 0, 0, 0, 0, 0,
         5, 10, 10, -20, -20, 10, 10, 5,
@@ -80,7 +80,7 @@ def evaluateScore():
         -30, -20, -10, 0, 0, -10, -20, -30,
         -50, -40, -30, -20, -20, -30, -40, -50]
 
-    #Number of each pieces based on current board state
+    #Number of each pieces on current board
     whitePawn = len(board.pieces(chess.PAWN, chess.WHITE))
     blackPawn = len(board.pieces(chess.PAWN, chess.BLACK))
     whiteKnight = len(board.pieces(chess.KNIGHT, chess.WHITE))
@@ -99,13 +99,13 @@ def evaluateScore():
     queenScore = sum([queenTable[i] for i in board.pieces(chess.QUEEN, chess.WHITE)]) - sum([queenTable[chess.square_mirror(i)] for i in board.pieces(chess.QUEEN, chess.BLACK)])
     
     if 100 * whitePawn + 300 * whiteKnight + 300 * whiteBishop + 500 * whiteRook + 900 * whiteQueen < 1400 and 100 * blackPawn + 300 * blackKnight + 300 * blackBishop + 500 * blackRook + 900 * blackQueen < 1400:
-        #If materialScore on both sides are less than 1400 (queen + rook = 900 + 500 = 1400), use kingEndTable
+        #If materialScore on both sides are less than 1400 (queen + rook = 900 + 500 = 1400) use kingEndTable
         kingScore = sum([kingEndTable[i] for i in board.pieces(chess.KING, chess.WHITE)]) - sum([kingEndTable[chess.square_mirror(i)] for i in board.pieces(chess.KING, chess.BLACK)])
     else:
         kingScore = sum([kingTable[i] for i in board.pieces(chess.KING, chess.WHITE)]) - sum([kingTable[chess.square_mirror(i)] for i in board.pieces(chess.KING, chess.BLACK)])
 
-    #Pawns are worth 100, knights 300, bishops 375, rooks 500 and queen 900
-    materialScore = 100 * (whitePawn - blackPawn) + 300 * (whiteKnight - blackKnight) + 375 * (whiteBishop - blackBishop) + 500 * (whiteRook - blackRook) + 900 * (whiteQueen - blackQueen)
+    #Pawns are worth 100, knights 300, bishops 300, rooks 500 and queen 900
+    materialScore = 100 * (whitePawn - blackPawn) + 300 * (whiteKnight - blackKnight) + 300 * (whiteBishop - blackBishop) + 500 * (whiteRook - blackRook) + 900 * (whiteQueen - blackQueen)
     totalScore = materialScore + pawnScore + knightScore + bishopScore + rookScore + queenScore + kingScore
 
     if board.turn: #Return positive score if White turn. Negative score if Black turn
@@ -154,7 +154,7 @@ def quiescence(alpha, beta):
 
     return alpha
 
-#Sort moves in ascending order based on their score
+#Sort moves in ascending order based on score
 def sortMoves(capturesOnly):
     
     moveValuePair = {}
@@ -178,36 +178,29 @@ def sortMoves(capturesOnly):
 def selectMove(depth):
 
     bestScore = -99999 #Initialize bestScore as worst possible White score
-    alpha = -100000 #Alpha is bestScore for the maximizing player (White). Initialize as worst possible White score
-    beta = 100000 #Beta is bestScore for the minimizing player (Black). Initialize as worst possible Black score
-
-    #For AI vs AI, if repeated moves, increase depth. For human vs AI, you can comment these 2 lines since humans usually won't repeat moves
-    if len(moveList) >= 4 and moveList[-4][:2] == moveList[-2][-2:] and moveList[-4][-2:] == moveList[-2][:2] and moveList[-3][:2] == moveList[-1][-2:] and moveList[-3][-2:] == moveList[-1][:2]:
-        depth += 1
+    alpha = -100000 #Alpha is bestScore for maximizing player (White). Initialize as worst possible White score
+    beta = 100000 #Beta is bestScore for minimizing player (Black). Initialize as worst possible Black score
     
     for move in board.legal_moves:
-        if board.is_castling(move): #Don't need to check castling rights if the move is a castle
+        #Skip move if threefold repetition for AI vs AI. For human vs AI you can comment these 5 lines since humans usually won't repeat moves
+        board.push(move)
+        if board.can_claim_threefold_repetition():
+            board.pop()
+            continue
+        board.pop()
+        if board.is_castling(move): #Don't need to check castling rights if move is a castle
             board.push(move)
-            if board.turn and len(board.pieces(chess.BISHOP, chess.BLACK)) == 1: #Subtract 50 (Value of half pawn) if player has only 1 bishop
-                moveScore = -negamax(-beta, -alpha, depth - 1) - 50
-            elif not board.turn and len(board.pieces(chess.BISHOP, chess.WHITE)) == 1:
-                moveScore = -negamax(-beta, -alpha, depth - 1) - 50
-            else:
-                moveScore = -negamax(-beta, -alpha, depth - 1)
+            moveScore = -negamax(-beta, -alpha, depth - 1)
         else:
             if board.turn:
-                castleRightsBefore = board.has_castling_rights(chess.WHITE) #Compare castle rights before and after. If before is true and after is false then the move prevents castling
+                castleRightsBefore = board.has_castling_rights(chess.WHITE) #Compare castle rights before and after. If before is true and after is false the move prevents castling
                 board.push(move)
                 castleRightsAfter = board.has_castling_rights(chess.WHITE)
-                numBishops = len(board.pieces(chess.BISHOP, chess.WHITE))
             else:
                 castleRightsBefore = board.has_castling_rights(chess.BLACK)
                 board.push(move)
                 castleRightsAfter = board.has_castling_rights(chess.BLACK)
-                numBishops = len(board.pieces(chess.BISHOP, chess.BLACK))
-            if numBishops == 1 and castleRightsBefore and not castleRightsAfter: #Subtract 100 (Value of 1 pawn) if player has only 1 bishop and if the move prevents castling
-                moveScore = -negamax(-beta, -alpha, depth - 1) - 100
-            elif castleRightsBefore and not castleRightsAfter: #Subtract 50 (Value of half pawn) if the move prevents castling
+            if castleRightsBefore and not castleRightsAfter: #Subtract 50 (Value of half pawn) if move prevents castling
                 moveScore = -negamax(-beta, -alpha, depth - 1) - 50
             else:
                 moveScore = -negamax(-beta, -alpha, depth - 1)
@@ -219,23 +212,25 @@ def selectMove(depth):
 
     return bestMove
 
-#Main Function
+#Main
 board = chess.Board()
-moveList = []
 display.start(board.fen())
 printEndMessage = False
 
 while not display.checkForQuit():
     if not board.is_game_over():
-        #Uncomment if you want to play vs AI by typing move. A move is starting square of piece you want to move followed by square where you want piece to move to. Ex: e2e4 moves pawn on e2 to e4
+        #Uncomment to play vs AI by typing move. A move is square of piece you want to move and square where you want piece to move to. Ex: e2e4 moves e2 pawn to e4. Put AIMove first to play as Black
         #playerMove = input()
-        #board.push(playerMove)
+        #board.push_san(playerMove)
         #display.update(board.fen())
-        AImove = selectMove(3) #Increase number for harder AI at the cost of it taking longer
+        #print("Player move:", playerMove)
+        AImove = selectMove(1) #Increase number for harder AI at the cost of it taking longer
+        if board.turn:
+            print("White move:", AImove)
+        else:
+            print("Black move:", AImove)
         board.push(AImove)
-        moveList.append(AImove.uci())
         display.update(board.fen())
-        print(AImove)
     elif not printEndMessage:
         if board.is_checkmate():
             if board.turn:
